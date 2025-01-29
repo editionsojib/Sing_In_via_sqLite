4cc4f47ddec277dde46e9054c685e4afa9a52ac3
sojib <sojibfara9486@gmail.com>
2025-01-29 18:18:08 +0600

    commit

---
+++ app/src/main/java/com/editions/sing_in_via_sqlite/HomeActivity.java
@@ -0,0 +1,35 @@
+package com.editions.sing_in_via_sqlite;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class HomeActivity extends AppCompatActivity {
+    Button log_outBtn_id;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_home);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        log_outBtn_id = findViewById(R.id.log_outBtn_id);
+
+        Toast.makeText(this, " To HomeScreen ", Toast.LENGTH_SHORT).show();
+
+        log_outBtn_id.setOnClickListener(v -> {
+           startActivity(new Intent(HomeActivity.this, ActivityLogin.class));
+        });
+    }
+}
